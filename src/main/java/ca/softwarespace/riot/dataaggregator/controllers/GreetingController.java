2b661f20fff30083942431bb12d89f1e6f9d70e9
Steve Mbiele <stevembiele@gmail.com>
2019-06-05 04:21:13 -0400

    Refactored the Match list aggregator to make it more flexible, added SL4J, and made the RIOT API call ASYNC.

--- src/main/java/ca/softwarespace/riot/dataaggregator/controllers/GreetingController.java
+++ src/main/java/ca/softwarespace/riot/dataaggregator/controllers/GreetingController.java
@@ -1,13 +1,23 @@
 package ca.softwarespace.riot.dataaggregator.controllers;
 
+import ca.softwarespace.riot.dataaggregator.services.MatchListService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingController {
 
+    private MatchListService matchListService;
+
+    public GreetingController(MatchListService matchListService) {
+        this.matchListService = matchListService;
+    }
+
     @GetMapping("/")
     public String greetings() {
+        matchListService.fetchMatchList("fastboyz");
+        matchListService.fetchMatchList("maytaro");
+        matchListService.fetchMatchList("yofranck99");
         return "Hello from";
     }
 }

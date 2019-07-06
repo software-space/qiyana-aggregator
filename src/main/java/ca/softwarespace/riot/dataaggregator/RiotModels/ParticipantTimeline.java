6bc4ee5b33d9eb5b9b1deffbc8dc4b03579cea55
Steve Mbiele <stevembiele@gmail.com>
2019-07-06 09:31:12 +0000

    Created most of the models

--- src/main/java/ca/softwarespace/riot/dataaggregator/RiotModels/ParticipantTimeline.java
+++ src/main/java/ca/softwarespace/riot/dataaggregator/RiotModels/ParticipantTimeline.java
@@ -1,21 +1,54 @@
 package ca.softwarespace.riot.dataaggregator.RiotModels;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
 import java.util.Map;
 
 /**
  * Author: Steve Mbiele
  * Date: 5/26/2019
  */
+@Entity
+@Table(name = "ParticipantTimeline")
 public class ParticipantTimeline {
 
+    @Id
+    @Column(name = "id")
+    @JsonIgnore
+    @GeneratedValue(generator = "participantTimeline_generator")
+    @SequenceGenerator(
+            name = "participantTimeline_generator",
+            sequenceName = "participantTimeline_sequence",
+            initialValue = 1000
+    )
+    private int id;
+
+    @Column
     private String lane;
+
+    @Column
     private int participantId;
+
+    @Column
     private String role;
+
+    @ElementCollection
     private Map<String, Double> csDiffPerMinDeltas;
+
+    @ElementCollection
     private Map<String, Double> goldPerMinDeltas;
+
+    @ElementCollection
     private Map<String, Double> creepsPerMinDeltas;
+
+    @ElementCollection
     private Map<String, Double> xpPerMinDeltas;
+
+    @ElementCollection
     private Map<String, Double> damageTakenDiffPerMinDeltas;
+
+    @ElementCollection
     private Map<String, Double> damageTakenPerMinDeltas;
 
     public ParticipantTimeline() {
@@ -34,6 +67,14 @@ public class ParticipantTimeline {
         this.xpPerMinDeltas = xpPerMinDeltas;
         this.damageTakenDiffPerMinDeltas = damageTakenDiffPerMinDeltas;
         this.damageTakenPerMinDeltas = damageTakenPerMinDeltas;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getLane() {

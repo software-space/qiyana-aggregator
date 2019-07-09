cdc318d28a7877e6954a8b3f6e3cd35e2079eb65
Steve Mbiele <mbielesteve@hotmail.com>
2019-07-09 13:20:25 -0400

    #3 Added the Orianna wrapper.
    The orianna wrapper was added, plus the support for mongo db, which means there was no need for PosgresSQL anymore as well as the old models. All those were also removed.

--- src/main/java/ca/softwarespace/qiyanna/dataaggregator/controllers/GreetingController.java
+++ src/main/java/ca/softwarespace/qiyanna/dataaggregator/controllers/GreetingController.java
@@ -1,7 +1,9 @@
 package ca.softwarespace.qiyanna.dataaggregator.controllers;
 
 import ca.softwarespace.qiyanna.dataaggregator.services.MatchListService;
+import com.merakianalytics.orianna.types.core.summoner.Summoner;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,11 +15,12 @@ public class GreetingController {
         this.matchListService = matchListService;
     }
 
-    @GetMapping("/")
-    public String greetings() {
-        matchListService.fetchMatchList("fastboyz");
-        matchListService.fetchMatchList("maytaro");
-        matchListService.fetchMatchList("yofranck99");
-        return "Hello from";
+    @GetMapping("/{summonerName}")
+    public String greetings(@PathVariable String summonerName) {
+//        matchListService.fetchMatchList("fastboyz");
+//        matchListService.fetchMatchList("maytaro");
+//        matchListService.fetchMatchList("yofranck99");
+        Summoner summoner = matchListService.oriannaTest(summonerName);
+        return  summoner.toJSON();
     }
 }
